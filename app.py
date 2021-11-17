0c2f4070e1e28766ce89a820abb43aa234f3ca8f
mouse3 <70779934+mouse3@users.noreply.github.com>
2021-11-17 20:17:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,84 +1,79 @@
-import socketserver 
-import socket, os
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad,unpad
-from Crypto.Random import get_random_bytes
-from binascii import unhexlify
-from secret import FLAG
+import string
+from itertools import product
+from time import time
+from numpy import loadtxt
 
 
-wlcm_msg ='########################################################################\n'+\
-		  '#                  Welcome to the Bank of the World                    #\n'+\
-		  '#             All connections are monitored and recorded               #\n'+\
-		  '#      Disconnect IMMEDIATELY if you are not an authorized user!       #\n'+\
-		  '########################################################################\n'
+def product_loop(password, generator):
+    for p in generator:
+        if ''.join(p) == password:
+            print('\nPassword:', ''.join(p))
+            return ''.join(p)
+    return False
 
 
-key = get_random_bytes(16)
-iv = get_random_bytes(16)
+def bruteforce(password, max_nchar=8):
+    """Password brute-force algorithm.
+    Parameters
+    ----------
+    password : string
+        To-be-found password.
+    max_nchar : int
+        Maximum number of characters of password.
+    Return
+    ------
+    bruteforce_password : string
+        Brute-forced password
+    """
+    print('1) Comparing with most common passwords / first names')
+    common_pass = loadtxt('probable-v2-top12000.txt', dtype=str)
+    common_names = loadtxt('middle-names.txt', dtype=str)
+    cp = [c for c in common_pass if c == password]
+    cn = [c for c in common_names if c == password]
+    cnl = [c.lower() for c in common_names if c.lower() == password]
+
+    if len(cp) == 1:
+        print('\nPassword:', cp)
+        return cp
+    if len(cn) == 1:
+        print('\nPassword:', cn)
+        return cn
+    if len(cnl) == 1:
+        print('\nPassword:', cnl)
+        return cnl
+
+    print('2) Digits cartesian product')
+    for l in range(1, 9):
+        generator = product(string.digits, repeat=int(l))
+        print("\t..%d digit" % l)
+        p = product_loop(password, generator)
+        if p is not False:
+            return p
+
+    print('3) Digits + ASCII lowercase')
+    for l in range(1, max_nchar + 1):
+        print("\t..%d char" % l)
+        generator = product(string.digits + string.ascii_lowercase,
+                            repeat=int(l))
+        p = product_loop(password, generator)
+        if p is not False:
+            return p
+
+    print('4) Digits + ASCII lower / upper + punctuation')
+    # If it fails, we start brute-forcing the 'hard' way
+    # Same as possible_char = string.printable[:-5]
+    all_char = string.digits + string.ascii_letters + string.punctuation
+
+    for l in range(1, max_nchar + 1):
+        print("\t..%d char" % l)
+        generator = product(all_char, repeat=int(l))
+        p = product_loop(password, generator)
+        if p is not False:
+            return p
 
 
-def encrypt_data(data):
-	padded = pad(data.encode(),16,style='pkcs7')
-	cipher = AES.new(key, AES.MODE_CBC,iv)
-	enc = cipher.encrypt(padded)
-	return enc.hex()
-
-def decrypt_data(encryptedParams):
-	cipher = AES.new(key, AES.MODE_CBC,iv)
-	paddedParams = cipher.decrypt( unhexlify(encryptedParams))
-	print(paddedParams)
-	if b'admin&password=g0ld3n_b0y' in unpad(paddedParams,16,style='pkcs7'):
-		return 1
-	else:
-		return 0
-
-def send_msg(s, msg):
-	enc = msg.encode()
-	s.send(enc)
-
-def main(s):
-	send_msg(s, 'username: ')
-	user = s.recv(4096).decode().strip()
-
-	send_msg(s, user +"'s password: " )
-	passwd = s.recv(4096).decode().strip()
-	
-	send_msg(s, wlcm_msg)
-
-	msg = 'logged_username=' + user +'&password=' + passwd
-
-	try:
-		assert('admin&password=g0ld3n_b0y' not in msg)
-	except AssertionError:
-		send_msg(s, 'You cannot login as an admin from an external IP.\nYour activity has been logged. Goodbye!\n')
-		raise
-
-	msg = 'logged_username=' + user +'&password=' + passwd
-	send_msg(s, "Leaked ciphertext: " + encrypt_data(msg)+'\n')
-	send_msg(s,"enter ciphertext: ")
-
-	enc_msg = s.recv(4096).decode().strip()
-	
-	try:
-		check = decrypt_data(enc_msg)
-	except Exception as e:
-		send_msg(s, str(e) + '\n')
-		s.close()
-
-	if check:
-		send_msg(s, 'Logged in successfully!\nYour flag is: '+ FLAG)
-		s.close()
-	else:
-		send_msg(s, 'Please try again.')
-		s.close()
-
-
-class TaskHandler(socketserver.BaseRequestHandler):
-	def handle(self):
-		main(self.request)
-
-if __name__ == '__main__':
-	socketserver.ThreadingTCPServer.allow_reuse_address = True
-	server = socketserver.ThreadingTCPServer(('0.0.0.0', 1337), TaskHandler)
-	server.serve_forever()
+# EXAMPLE
+start = time()
+bruteforce('sunshine') # Try with '123456' or '751345' or 'test2018'
+end = time()
+print('Total time: %.2f seconds' % (end - start))
